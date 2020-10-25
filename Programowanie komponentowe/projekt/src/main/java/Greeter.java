11f4baa36f95554eec8a5b3e2b48c71506543153
229879 Gebel Michał <229879@edu.p.lodz.pl>
2020-10-25 20:03:26 +0100

    usuwanie

--- Programowanie komponentowe/projekt/src/main/java/Greeter.java
+++
@@ -1,5 +0,0 @@
-public class Greeter {
-    public String greet( String name) {
-        return "Hello world, " + name;
-    }
-}
--- Programowanie komponentowe/projekt/src/main/java/Hit.java
+++
@@ -1,6 +0,0 @@
-public class Hit {
-    public static void main ( String[] args ){
-        Greeter greeter = new Greeter();
-        System.out.println(greeter.greet("Michal"));
-    }
-}
