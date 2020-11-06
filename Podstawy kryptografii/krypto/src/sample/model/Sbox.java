903c80532928cc44828b8a2253c424c79c09306d
229879 Gebel Michał <229879@edu.p.lodz.pl>
2020-11-06 23:11:18 +0100

    blisko

---
+++ Podstawy kryptografii/krypto/src/sample/model/Sbox.java
@@ -0,0 +1,104 @@
+package sample.model;
+
+public class Sbox {
+
+    byte[] box1 = new byte[]{14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7,
+            0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8,
+            4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0,
+            15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13};
+
+    byte[] box2 = new byte[]{15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10,
+            3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5,
+            0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15,
+            13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9};
+
+    byte[] box3 = new byte[]{10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8,
+            13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1,
+            13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7,
+            1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12};
+
+    byte[] box4  = new byte[]{7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15,
+            13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9,
+            10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4,
+            3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14};
+
+    byte[] box5  = new byte[]{2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9,
+            14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6,
+            4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14,
+            11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3};
+
+    byte[] box6  = new byte[]{12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11,
+            10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8,
+            9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6,
+            4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13};
+
+    byte[] box7  = new byte[]{4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1,
+            13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6,
+            1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2,
+            6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12};
+
+    byte[] box8  = new byte[]{13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7,
+            1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2,
+            7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8,
+            2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11};
+    byte[][] boxes = new byte[][]{box1, box2, box3, box4, box5, box6, box7, box8};
+
+    public BitArray transformArray(BitArray array){
+        BitArray array2 = array.divide(6, 42);
+        BitArray array3 = array2.divide(6, 36);
+        BitArray array4 = array3.divide(6, 30);
+        BitArray array5 = array4.divide(6, 24);
+        BitArray array6 = array5.divide(6, 18);
+        BitArray array7 = array6.divide(6, 12);
+        BitArray array8 = array7.divide(6, 6);
+
+        BitArray[] arrays = new BitArray[]{array, array2, array3, array4, array5, array6, array7, array8};
+
+        BitArray result = new BitArray(new boolean[]{});
+        for(int i = 0; i < 8; i++){
+            result = result.connect(tranform(arrays[i], i));
+        }
+        return result;
+    }
+
+    private BitArray tranform(BitArray array, int number){
+
+        byte coordX = bitsToByte(new boolean[]{array.getBits()[0], array.getBits()[5]});
+        byte coordY = bitsToByte(new boolean[]{array.getBits()[1], array.getBits()[2], array.getBits()[3], array.getBits()[4]});
+        return byteToBitArray(boxes[number][coordX*16+coordY]);
+
+    }
+
+    private byte bitsToByte (boolean[] booleans){
+        byte result = 0;
+        for (int i = 0; i < booleans.length; i++){
+            if (booleans[i]){
+                result += Math.pow(2, i);
+            }
+        }
+        return result;
+    }
+    private BitArray byteToBitArray (int b){
+        boolean[] array = new boolean[4];
+        int pom = b;
+        for (int i = 0; i < 4; i++){
+            if (pom%2 == 0){
+                array[i] = false;
+            }else{
+                array[i] = true;
+            }
+            pom = pom/2;
+        }
+        boolean nieMogeJuz = array[3];
+        array[3] = array[0];
+        array[0] = nieMogeJuz;
+
+        nieMogeJuz = array[1];
+        array[1] = array[2];
+        array[2] = nieMogeJuz;
+
+        return new BitArray(array);
+    }
+
+
+}
