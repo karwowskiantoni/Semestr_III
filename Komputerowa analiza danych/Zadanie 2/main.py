b22e3e4fa86e5b80918ed95b68c6c2b4c6509736
229879 Gebel Michał <229879@edu.p.lodz.pl>
2020-12-06 21:37:30 +0100

    kadzior

--- Komputerowa analiza danych/Zadanie 2/main.py
+++ Komputerowa analiza danych/Zadanie 2/main.py
@@ -1,7 +1,8 @@
-import math
 import pandas as pd
 import numpy as np
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
 
 columnsOne = ["x", "y"]
 dataFrameOne = pd.read_csv("data1.csv", names=columnsOne)
@@ -9,10 +10,6 @@ dataFrameTwo = pd.read_csv("data2.csv", names=columnsOne)
 columnsTwo = ["x1", "x2", 'y']
 dataFrameThree = pd.read_csv("data3.csv", names=columnsTwo)
 dataFrameFour = pd.read_csv("data4.csv", names=columnsTwo)
-
-
-
-
 
 def plotLine(y, x, colour):
     plt.plot(x, y, colour)
@@ -45,6 +42,7 @@ def gradientDescent(x, y, thetas, alpha):
     return thetas
 
 if __name__ == '__main__':
+    mpl.use("TkAgg")
                                                # DANE 1
     # docArgumentsOne = np.array(dataFrameOne["x"].tolist())
     # valuesOne = np.array(dataFrameOne["y"].tolist())
@@ -79,58 +77,99 @@ if __name__ == '__main__':
     # plotLine(readyThetasThree, docArgumentsOne, "k")
     # plt.show()
                                               # DANE 2
-    docArgumentsTwo = np.array(dataFrameTwo["x"].tolist())
-    valuesTwo = np.array(dataFrameTwo["y"].tolist())
-
-    argumentsOne = np.array(list(map(lambda x: [x], docArgumentsTwo)))
-    modelOneTheta = np.random.rand(1)
-
-    argumentsTwo = np.array(list(map(lambda x: [x, 1], docArgumentsTwo)))
-    modelTwoThetas = np.random.rand(2)
-
-    argumentsThree = np.array(list(map(lambda x: [pow(x, 2), math.sin(x), 1], docArgumentsTwo)))
-    modelThreeThetas = np.random.rand(3)
-
-    alpha = 0.001
-
-    modelOneTheta = gradientDescent(argumentsOne, valuesTwo, modelOneTheta, alpha)
-    readyThetasOne = []
-    for x in argumentsOne:
-        readyThetasOne.append(fX(x, modelOneTheta))
-
-    modelTwoThetas = gradientDescent(argumentsTwo, valuesTwo, modelTwoThetas, alpha)
-    readyThetasTwo = []
-    for x in argumentsTwo:
-        readyThetasTwo.append(fX(x, modelTwoThetas))
-
-    modelThreeThetas = gradientDescent(argumentsThree, valuesTwo, modelThreeThetas, alpha)
-    readyThetasThree = []
-    for x in argumentsThree:
-        readyThetasThree.append(fX(x, modelThreeThetas))
-
-    plt.plot(docArgumentsTwo, valuesTwo, 'go')
-    plotLine(readyThetasOne, docArgumentsTwo, "r")
-    plotLine(readyThetasTwo, docArgumentsTwo, "b")
-    plotLine(readyThetasThree, docArgumentsTwo, "k")
-    plt.show()
+    # docArgumentsTwo = np.array(dataFrameTwo["x"].tolist())
+    # valuesTwo = np.array(dataFrameTwo["y"].tolist())
+    #
+    # argumentsOne = np.array(list(map(lambda x: [x], docArgumentsTwo)))
+    # modelOneTheta = np.random.rand(1)
+    #
+    # argumentsTwo = np.array(list(map(lambda x: [x, 1], docArgumentsTwo)))
+    # modelTwoThetas = np.random.rand(2)
+    #
+    # argumentsThree = np.array(list(map(lambda x: [pow(x, 2), math.sin(x), 1], docArgumentsTwo)))
+    # modelThreeThetas = np.random.rand(3)
+    #
+    # alpha = 0.001
+    #
+    # modelOneTheta = gradientDescent(argumentsOne, valuesTwo, modelOneTheta, alpha)
+    # readyThetasOne = []
+    # for x in argumentsOne:
+    #     readyThetasOne.append(fX(x, modelOneTheta))
+    #
+    # modelTwoThetas = gradientDescent(argumentsTwo, valuesTwo, modelTwoThetas, alpha)
+    # readyThetasTwo = []
+    # for x in argumentsTwo:
+    #     readyThetasTwo.append(fX(x, modelTwoThetas))
+    #
+    # modelThreeThetas = gradientDescent(argumentsThree, valuesTwo, modelThreeThetas, alpha)
+    # readyThetasThree = []
+    # for x in argumentsThree:
+    #     readyThetasThree.append(fX(x, modelThreeThetas))
+    #
+    # plt.plot(docArgumentsTwo, valuesTwo, 'go')
+    # plotLine(readyThetasOne, docArgumentsTwo, "r")
+    # plotLine(readyThetasTwo, docArgumentsTwo, "b")
+    # plotLine(readyThetasThree, docArgumentsTwo, "k")
+    # plt.show()
     #                                           Dane 3
     # docArguments1Three = np.array(dataFrameThree["x1"].tolist())
     # docArguments2Three = np.array(dataFrameThree["x2"].tolist())
     # valuesThree = np.array(dataFrameThree["y"].tolist())
+    #
     # argumentsOne = np.array(list(map(lambda x1, x2: [x1, x2, 1], docArguments1Three, docArguments2Three)))
-    # modelOneTheta = np.random.rand(3)
-    # #print(argumentsOne)
+    # modelOneThetas = np.random.rand(3)
+    #
     # argumentsTwo = np.array(list(map(lambda x1, x2: [pow(x1, 2), x1 * x2, pow(x2, 2), x1, x2, 1]
     #                                  , docArguments1Three, docArguments2Three)))
     # modelTwoThetas = np.random.rand(6)
+    #
+    # alpha = 0.01
+
+    # modelOneThetas = gradientDescent(argumentsOne, valuesThree, modelOneThetas, alpha)
+    # readyThetasOne = []
+    # for x in argumentsOne:
+    #     readyThetasOne.append(fX(x, modelOneThetas))
+
+    # modelTwoThetas = gradientDescent(argumentsTwo, valuesThree, modelTwoThetas, alpha)
+    # readyThetasTwo = []
+    # for x in argumentsTwo:
+    #     readyThetasTwo.append(fX(x, modelTwoThetas))
+    #
+    #
+    # fig = plt.figure()
+    # ax=plt.axes(projection="3d")
+    # ax.plot(docArguments1Three, docArguments2Three, valuesThree, "go", markersize=3)
+    # # ax.plot_trisurf(docArguments1Three, docArguments2Three, readyThetasOne, alpha=0.4, edgecolor='none',color='b')
+    # ax.plot_trisurf(docArguments1Three, docArguments2Three, readyThetasTwo, alpha=0.4, edgecolor='none',color='r')
+    # plt.show()
 
     #                                           Dane 4
-    # docArguments1Four = np.array(dataFrameFour["x1"].tolist())
-    # docArguments2Four = np.array(dataFrameFour["x2"].tolist())
-    # valuesThree = np.array(dataFrameFour["y"].tolist())
-    # argumentsOne = np.array(list(map(lambda x1, x2: [x1, x2, 1], docArguments1Four, docArguments2Four)))
-    # modelOneTheta = np.random.rand(3)
-    #
-    # argumentsTwo = np.array(list(map(lambda x1, x2: [pow(x1, 2), x1 * x2, pow(x2, 2), x1, x2, 1]
-    #                                  , docArguments1Four, docArguments2Four)))
-    # modelTwoThetas = np.random.rand(6)
+    docArguments1Four = np.array(dataFrameFour["x1"].tolist())
+    docArguments2Four = np.array(dataFrameFour["x2"].tolist())
+    valuesThree = np.array(dataFrameFour["y"].tolist())
+    argumentsOne = np.array(list(map(lambda x1, x2: [x1, x2, 1], docArguments1Four, docArguments2Four)))
+    modelOneTheta = np.random.rand(3)
+
+    argumentsTwo = np.array(list(map(lambda x1, x2: [pow(x1, 2), x1 * x2, pow(x2, 2), x1, x2, 1]
+                                     , docArguments1Four, docArguments2Four)))
+    modelTwoThetas = np.random.rand(6)
+
+    alpha = 0.01
+
+    # modelOneThetas = gradientDescent(argumentsOne, valuesThree, modelOneThetas, alpha)
+    # readyThetasOne = []
+    # for x in argumentsOne:
+    #     readyThetasOne.append(fX(x, modelOneThetas))
+
+    modelTwoThetas = gradientDescent(argumentsTwo, valuesThree, modelTwoThetas, alpha)
+    readyThetasTwo = []
+    for x in argumentsTwo:
+        readyThetasTwo.append(fX(x, modelTwoThetas))
+
+
+    fig = plt.figure()
+    ax=plt.axes(projection="3d")
+    ax.plot(docArguments1Four, docArguments2Four, valuesThree, "go", markersize=3)
+    # ax.plot_trisurf(docArguments1Three, docArguments2Three, readyThetasOne, alpha=0.4, edgecolor='none',color='b')
+    ax.plot_trisurf(docArguments1Four, docArguments2Four, readyThetasTwo, alpha=0.4, edgecolor='none',color='r')
+    plt.show()
