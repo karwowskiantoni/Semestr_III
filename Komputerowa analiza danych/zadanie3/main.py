b18f7c25d9d0760ec419b32a922a800e7d2a29e8
229879 Gebel Michał <229879@edu.p.lodz.pl>
2021-01-17 19:47:27 +0100

    dart

--- Komputerowa analiza danych/zadanie3/main.py
+++ Komputerowa analiza danych/zadanie3/main.py
@@ -1,113 +1,88 @@
 import numpy as np
 from numpy.random.mtrand import random
 
-# zakładamy że każda sieć ma tylko jedną warstwę
-# wejscie - tablica numpy postaci X[x1, x2, ... , xn]
-# wyjscie - tablica numpy postaci Y[y1, y2, ... , yn]
-# przewidywane wyjscie - tablica numpy postaci Z[z1, z2, ... , zn]
 
-# XOR INPUT=2 OUTPUT=1
-
-#       1        2       3
-#       x1 ->    o ->    y     (y1 - z1)^2 = blad sredniokwadratowy       |bląd|    (błąd)^2
-#       x2 ->    o ->
-#                o ->
-# x0=1
-# X1=2
-# X2=3
-#
-#multiple layer perceptron
-
-        #  1
-        #  1
-
-        #   0,43  0,92
-        #   0,12  0,15
-        #   0,92  0,34
-
-        #  0,75  0,64  0,23
-
-# funkcja_aktywacji(funkcja_kosztu)
-# waga = waga-pochodna funkcji kosztu * współczynnik uczenia
+def funkcja_sigmoidalna(x):
+    return 1 / (1 + np.exp(-x))
 
 
-
-#
-#
-# def pochodna_funkcji_aktywacji(x):
-#     return x*(1-x)
-#
-#
-#
-
-
-# def propagacja_wsteczna():
-#     waga = 2*(BLAD)*pochodna_funkcji_aktywacji(x)
+def pochodna_funkcji_sigmoidalnej(wartosc_w_punkcie):
+    return wartosc_w_punkcie * (1 - wartosc_w_punkcie)
 
 
 class Warstwa:
-    # liczba neuronow: hah, no liczba neuronow
-    # wejscie: x tablica jednowymiarowa zawierająca wyjscie poprzedniej warstwy [wartosci_wejsciowe]
-    # wagi: W tablica dwuwymiarowa zawierajaca w kazdym wierszu wszystkie wagi neuronow [neurony, wagi_neuronow]
-    wejscie = 0
-    wagi = 0
+    def __init__(self, liczba_wejsc, liczba_neuronow, funkcja_aktywacji, pochodna_funkcji_aktywacji):
+        self.wagi = (np.array([np.random.rand(liczba_wejsc) for _ in range(liczba_neuronow)])-0.5)
+        self.funkcja_aktywacji = funkcja_aktywacji
+        self.pochodna_funkcji_aktywacji = pochodna_funkcji_aktywacji
 
-    def __init__(self, liczba_wejsc, liczba_neuronow):
-        self.wagi = np.array([np.random.rand(liczba_wejsc) for _ in range(liczba_neuronow)])
-        print(self.wagi)
-
-    def suma_wazona(self):
-        return np.matmul(self.wagi, self.wejscie)
-
-    #
-    # [1    [[w11, w12, w13, w14]         [suma W1*wejscie             w11*1 + w12*2 + w13*3 + w14*4
-    #  2     [w21, w22, w23, w24]          suma W2*wejscie
-    #  3     [w31, w32, w33, w34]          suma W3*wejscie
-    #  4]    [w41, w42, w43, w44]]         suma W4*wejscie]
-
-
-    def funkcja_aktywacji(self, wejscie):
+    def funkcja_wyjscia(self, wejscie):
         self.wejscie = wejscie
-        return 1/(1 + np.exp(-self.suma_wazona()))
+        self.wyjscie_przed_aktywacja = np.matmul(self.wagi, self.wejscie)
+        self.wyjscie = self.funkcja_aktywacji(self.wyjscie_przed_aktywacja)
+        return self.wyjscie
 
-    def pochodna_funkcji_aktywacji(self, wejscie):
-        return np.multiply(wejscie, 1-wejscie)
+    def pochodna_funkcji_wyjscia_po_wejsciach(self):
+        return np.transpose(self.wagi) * self.pochodna_funkcji_aktywacji(self.wyjscie)
+
+    def oblicz_blad_warstwy_poprzedniej(self, blad_warstwy):
+        return np.matmul(self.pochodna_funkcji_wyjscia_po_wejsciach(), blad_warstwy)
 
 
-class Siec_neuronowa:
-    warstwa_ukryta = 0
-    warstwa_koncowa = 0
-    wejscia = 0
-    wyjscia = 0
-    # wejscia : X tablica dwuwymiarowa zawierajaca w kazdym wierszu wszystkie wejscia jednego przypadku uczacego
-    # wyjscia : Y tablica dwuwymiarowa zawierajaca w kazdym wierszu wszystkie wyjscia jednego przypadku uczacego
 
-    def __init__(self, wejscia, wyjscia, liczba_neuronow_w_warstwie_ukrytej):
-        self.warstwa_ukryta = Warstwa(len(wejscia[0]), liczba_neuronow_w_warstwie_ukrytej)
-        self.warstwa_koncowa = Warstwa(liczba_neuronow_w_warstwie_ukrytej, len(wyjscia[0]))
-        self.wejscia = wejscia
-        self.wyjscia = wyjscia
+    def pochodna_funkcji_wyjscia_po_wagach(self):
+        return np.reshape(self.pochodna_funkcji_aktywacji(self.wyjscie), (len(self.wyjscie), 1)) * self.wejscie
+        # return self.wejscie
 
-    def propagacja_w_przod(self, numer_wejscia):
-        return self.warstwa_koncowa.funkcja_aktywacji(self.warstwa_ukryta.funkcja_aktywacji(self.wejscia[numer_wejscia]))
+    def zmiana_gradientu(self, wspolczynnik_uczenia, blad_warstwy):
+        self.wagi = self.wagi - wspolczynnik_uczenia * self._gradient(blad_warstwy)
 
-    def propagacja_w_tyl(self):
-        return 0
+    def _gradient(self, blad_warstwy):
+        return np.reshape(blad_warstwy, (len(self.wyjscie), 1)) * self.pochodna_funkcji_wyjscia_po_wagach()
+        # return np.matmul(np.transpose(self.pochodna_funkcji_wyjscia_po_wagach()), blad_warstwy)   # gradient
 
-    # def funkcja_kosztu(self, Y, Z):
-    #     return np.sum((Y - Z) ** 2) / (len(Z) * 2)
 
-    def pochodna_funkcji_kosztu(self, X, Y, Z)):
-        return np.matmul(np.transpose(X), Y - Z) / len(Y)
-        2 * (BLAD) * pochodna_funkcji_aktywacji(x)
+class SiecNeuronowa:
+    def __init__(self, liczba_wejsc, liczba_wyjsc, liczba_neuronow_w_warstwie_ukrytej):
+        self.warstwa_ukryta = Warstwa(liczba_wejsc, liczba_neuronow_w_warstwie_ukrytej,funkcja_sigmoidalna, pochodna_funkcji_sigmoidalnej)
+        self.warstwa_koncowa = Warstwa(liczba_neuronow_w_warstwie_ukrytej, liczba_wyjsc, funkcja_sigmoidalna, pochodna_funkcji_sigmoidalnej)
 
-    # def uczenie(self):
+    def werdykt(self, wejscie):
+        obliczone = self.propagacja_w_przod(wejscie)
+        wynik = obliczone
+        for i in range(len(obliczone)):
+            if obliczone[i] > 0.5:
+                wynik[i] = 1
+            else:
+                wynik[i] = 0
+        return wynik
+
+    def propagacja_w_przod(self, wejscie):
+        return self.warstwa_koncowa.funkcja_wyjscia(self.warstwa_ukryta.funkcja_wyjscia(wejscie))
+
+
+    def propagacja_wstecz(self, wejscia, wyjscia):
+        for epoki in range(5000):
+            indexes = [0, 1, 2, 3]
+            np.random.shuffle(indexes)
+            for i in indexes:
+                blad_warstwy_koncowej = self.propagacja_w_przod(wejscia[i]) - wyjscia[i]
+                blad_warstwy_ukrytej = self.warstwa_koncowa.oblicz_blad_warstwy_poprzedniej(blad_warstwy_koncowej)
+                self.warstwa_koncowa.zmiana_gradientu(0.1, blad_warstwy_koncowej)
+                self.warstwa_ukryta.zmiana_gradientu(0.1, blad_warstwy_ukrytej)
+                print(blad_warstwy_koncowej)
 
 
 if __name__ == '__main__':
-    X_zad1 = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
-    Y_zad1 = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
-    X_zad2 = [[0, 0], [1, 0], [0, 1], [1, 1]]
-    Y_zad2 = [[0], [1], [1], [0]]
-    neronek = Siec_neuronowa(X_zad2, Y_zad2, 3)
-    print(neronek.propagacja_w_przod(1))
+    X_zad1 = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+    Y_zad1 = np.array([[1, 1, 0, 1], [1, 1, 1, 0], [0, 1, 1, 1], [1, 0, 1, 1]])
+    X_zad2 = np.array([[0, 0], [1, 0], [0, 1], [1, 1]])
+    Y_zad2 = np.array([[0], [1], [1], [0]])
+    neronek = SiecNeuronowa(4, 4, 3)
+    neronek.propagacja_wstecz(X_zad1, Y_zad1)
+    print("neronek stwierdzil ze wynik pownien wygladac tak:")
+    print(neronek.werdykt(X_zad1[0]))
+    print(neronek.werdykt(X_zad1[1]))
+    print(neronek.werdykt(X_zad1[2]))
+    print(neronek.werdykt(X_zad1[3]))
+
